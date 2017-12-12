a7f417b963dbbd238bd25bb6d0f8082bcc8f4f4a
Chris Nolan <chrisdude7au@gmail.com>
2017-12-12 13:22:49 +1100

    Changing to my name in all fields

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
-      url='http://github.com/Samreay/WorkshopExample',
-      author='Samuel Hinton',
-      author_email='samuelreay@gmail.com',
+      url='http://github.com/chrisdude7au/WorkshopExample',
+      author='Chris Nolan',
+      author_email='chrisdude7au@gmail.com',
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages(exclude=('tests', 'doc', 'data'))
